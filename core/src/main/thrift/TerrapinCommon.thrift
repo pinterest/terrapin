5a75f28d65a3cd2b218a1e40786cd32ccc7e6b96
Varun <varun@pinterest.com>
2015-09-09 23:14:18 -0700

    Add apache 2 license.

--- core/src/main/thrift/TerrapinCommon.thrift
+++ core/src/main/thrift/TerrapinCommon.thrift
@@ -1,8 +1,23 @@
 /**
- * Thrift definitions for communication between the terrapin client library
- * and the terrapin server. These are for internal use by the client library
- * only which knows about the sharding of the data and can appropriately
- * route the requests.
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Common thrift definitions such as thrift structures/error codes used
+ * for both internal communication and by the terrapin thrift server.
  */
 
 namespace java com.pinterest.terrapin.thrift.generated
